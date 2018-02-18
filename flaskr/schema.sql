436360e06d724e10ba825e33985da66089951856
Ryuki Wada <xim.ryuki@gmail.com>
2018-02-19 00:08:46 +0900

    fix indent style

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -12,9 +12,3 @@ create table users (
   username string not null unique,
   password string not null
 );
-
-/*
-このスキーマでは、テーブル（entries）を作成し、 id 、 title 、 text のカラムがあります。
-id カラムはPrimaryKeyが自動で付加されます。 title 、 text は空白はNGです。
-#sqlite3 /tmp/flaskr.db < schema.sql
-*/
